f2059cde8dbf531592f36ea06f6d296ee2a2cdff
dima-gdp <dima5620g@gmail.com>
2020-08-08 22:48:42 +0300

    6 modul upd

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import products from '@/data/products';
 
 Vue.use(Vuex);
 
@@ -21,6 +22,25 @@ export default new Vuex.Store({
           amount,
         });
       }
+    },
+    updateCartProductAmount(state, { productId, amount }) {
+      const item = state.cartProducts.find((el) => el.productId === productId);
+      if (item && amount > 0) item.amount = amount;
+    },
+    deleteCartProduct(state, productId) {
+      state.cartProducts = state.cartProducts.filter((el) => el.productId !== productId);
+    },
+  },
+  getters: {
+    cartDetailProducts(state) {
+      return state.cartProducts.map((el) => ({
+        ...el,
+        product: products.find((item) => item.id === el.productId),
+      }
+      ));
+    },
+    cartTotalPrice(state, getters) {
+      return getters.cartDetailProducts.reduce((acc, item) => (item.product.price * item.amount) + acc, 0);
     },
   },
 });
